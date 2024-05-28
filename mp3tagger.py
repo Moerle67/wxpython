f80e857dd6806a9046d8f9ebce0f9b6111937d50
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-05-28 10:35:20 +0200

    menubar

--- mp3tagger.py
+++ mp3tagger.py
@@ -39,7 +39,28 @@ class Mp3Frame(wx.Frame):
         self.Show()
 
     def create_menu(self):
-        menu_bar
+        menu_bar = wx.MenuBar()
+        file_menu = wx.Menu()
+        open_folder_menu_item = file_menu.Append(
+            wx.ID_ANY, 'Open Folder', 
+            'Open a folder with MP3s'
+
+        )
+        menu_bar.Append(file_menu, '&File')
+        self.Bind(
+            event = wx.EVT_MENU,
+            handler = self.on_open_folder,
+            source = open_folder_menu_item,
+        )
+        self.SetMenuBar(menu_bar)
+
+    def on_open_folder(self, event):
+        title = 'Choose a directory:'
+        dlg = wx.DirDialog(self, title,
+                           style = wx.DD_DEFAULT_STYLE)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.panel.update_mp3_listing(dlg.GetPath())
+        dlg.Destroy()
 
 if __name__ == '__main__':
     app = wx.App(False)
